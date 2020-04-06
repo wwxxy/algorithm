a7fcc64a7f0edc74675c5ed360d0921c95726f3e
wxy <37918595+wwxxy@users.noreply.github.com>
2020-04-06 13:41:14 +0800

    Update bitree.c

--- data_structure/tree/binary_tree/bitree.c
+++ data_structure/tree/binary_tree/bitree.c
@@ -17,14 +17,12 @@ void preBiTree(const BiTree *t)
     BiTree *stack[STACK_SIZE] = {NULL};
     int top = -1;
 
-    while(temp != NULL || top != -1){  //当且仅有当前节点为空且栈为空时，结束
-        //当前节点存在，存入栈中，并访问左子树；
-        if(temp != NULL){   //节点非空，两种情况，空栈或非空栈
+    while(temp != NULL || top != -1){  
+        if(temp != NULL){
             printf("%d ", temp->data);
             stack[++top] = temp;
             temp = temp->lchild;
-        }else{ //节点为空，栈非空
-            //当前节点不存在，就出栈，并通过栈顶节点访问右子树
+        }else{
             temp = stack[top--];
             temp = temp->rchild;
         }
@@ -55,22 +53,22 @@ void postBiTree(const BiTree *t)
 {
     BiTree *temp = t;
     BiTree *stack[STACK_SIZE] = {NULL};
-    int flag[STACK_SIZE] = {0}; ////记录每个节点访问次数栈
+    int flag[STACK_SIZE] = {0}; 
     int top = -1;
 
     while(temp != NULL || top != -1){
         //left tree Traversal
-        if(temp != NULL){ //第一次访问，flag置1，入栈
+        if(temp != NULL){ 
             stack[++top] = temp;
             flag[top] = 1;
             temp = temp->lchild;
         }else{
-            if(flag[top] == 1){ ////第二次访问，flag置2，取栈顶元素但不出栈
+            if(flag[top] == 1){ 
                 temp = stack[top];
                 flag[top]++;
                 temp = temp->rchild;
             }else{
-                temp = stack[top--]; ////第三次访问，出栈
+                temp = stack[top--];
                 printf("%d ", temp->data);
                 temp = NULL;
             }
